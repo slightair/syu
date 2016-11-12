92842ce2d7a5a524a45bcbeb0ee2c1581a0a5e3e
slightair <arksutite@gmail.com>
2016-11-12 12:59:53 +0900

    Create project

---
+++ Syu/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Syu
+//
+//  Created by slightair on 2016/11/12.
+//  Copyright © 2016年 slightair. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Syu/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Syu
+//
+//  Created by slightair on 2016/11/12.
+//  Copyright © 2016年 slightair. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
