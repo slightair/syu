59b00130252a3518bcf491f813810b9a28308295
slightair <arksutite@gmail.com>
2016-11-23 13:03:58 +0900

    Turn on applicationShouldTerminateAfterLastWindowClosed

--- Syu/AppDelegate.swift
+++ Syu/AppDelegate.swift
@@ -1,26 +1,8 @@
-//
-//  AppDelegate.swift
-//  Syu
-//
-//  Created by slightair on 2016/11/12.
-//  Copyright © 2016年 slightair. All rights reserved.
-//
-
 import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
-
 }
-
